05d37b17ffc5fdc7d858da1432ed3bc896b2254b
vishwas-icodelabs <vishwas.kamal@icodelabs.co>
2025-10-07 11:29:38 +0530

    listing UI

--- src/containers/ListingPage/UserCard/UserCard.js
+++ src/containers/ListingPage/UserCard/UserCard.js
@@ -145,17 +145,22 @@ const UserCard = props => {
   return (
     <div className={classes}>
       <div className={css.content}>
-        <AvatarLarge className={css.avatar} user={user} />
-        <div className={css.info}>
-          <div className={css.headingRow}>
-            <FormattedMessage id="UserCard.heading" values={{ name: displayName }} />
-            {editProfileDesktop}
+        <div className={css.authContentContainer}>
+          <div className={css.flexCont}>
+            <AvatarLarge className={css.avatar} user={user} />
+            <div className={css.info}>
+              <div className={css.headingRow}>
+                <FormattedMessage id="UserCard.heading" values={{ name: displayName }} />
+                {editProfileDesktop}
+              </div>
+              {links}
+            </div>
           </div>
           {hasBio ? <ExpandableBio className={css.desktopBio} bio={bio} /> : null}
-          {links}
+          {hasBio ? <ExpandableBio className={css.mobileBio} bio={bio} /> : null}
         </div>
+
       </div>
-      {hasBio ? <ExpandableBio className={css.mobileBio} bio={bio} /> : null}
     </div>
   );
 };
