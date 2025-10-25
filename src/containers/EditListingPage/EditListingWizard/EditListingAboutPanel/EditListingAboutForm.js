c75adc0fec10cf73866ca2428b134da1296a1903
Ankit <ankit.kumar@icodelabs.co>
2025-10-25 23:57:06 +0530

    update code for latest chages

--- src/containers/EditListingPage/EditListingWizard/EditListingAboutPanel/EditListingAboutForm.js
+++ src/containers/EditListingPage/EditListingWizard/EditListingAboutPanel/EditListingAboutForm.js
@@ -1,174 +1,119 @@
 import React from 'react';
-import { Form as FinalForm } from 'react-final-form';
+import { Field, Form as FinalForm } from 'react-final-form';
 import arrayMutators from 'final-form-arrays';
+import { FieldArray } from 'react-final-form-arrays';
 import classNames from 'classnames';
-
-// Import configs and util modules
-import appSettings from '../../../../config/settings';
 import { FormattedMessage, useIntl } from '../../../../util/reactIntl';
 import * as validators from '../../../../util/validators';
-import { formatMoney } from '../../../../util/currency';
-import { types as sdkTypes } from '../../../../util/sdkLoader';
-import { FIXED, isBookingProcess } from '../../../../transactions/transaction';
-
-// Import shared components
-import {
-  Button,
-  Form,
-  FieldCurrencyInput,
-  FieldTextInput,
-  FieldSelect,
-} from '../../../../components';
-
-import BookingPriceVariants from '../EditListingPricingPanel/BookingPriceVariants';
+import { Button, Form, FieldTextInput } from '../../../../components';
 import StartTimeInterval from '../EditListingPricingPanel/StartTimeInverval';
-
-// Import modules from this directory
 import css from './EditListingAboutForm.module.css';
-
-const { Money } = sdkTypes;
-
-const getPriceValidators = (listingMinimumPriceSubUnits, marketplaceCurrency, intl) => {
-  const priceRequiredMsgId = { id: 'EditListingPricingForm.priceRequired' };
-  const priceRequiredMsg = intl.formatMessage(priceRequiredMsgId);
-  const priceRequired = validators.required(priceRequiredMsg);
-
-  const minPriceRaw = new Money(listingMinimumPriceSubUnits, marketplaceCurrency);
-  const minPrice = formatMoney(intl, minPriceRaw);
-  const priceTooLowMsgId = { id: 'EditListingPricingForm.priceTooLow' };
-  const priceTooLowMsg = intl.formatMessage(priceTooLowMsgId, { minPrice });
-  const minPriceRequired = validators.moneySubUnitAmountAtLeast(
-    priceTooLowMsg,
-    listingMinimumPriceSubUnits
-  );
-
-  return listingMinimumPriceSubUnits
-    ? validators.composeValidators(priceRequired, minPriceRequired)
-    : priceRequired;
-};
 
 const ErrorMessages = props => {
   const { fetchErrors } = props;
   const { updateListingError, showListingsError } = fetchErrors || {};
-
   return (
     <>
       {updateListingError ? (
         <p className={css.error}>
-          <FormattedMessage id="EditListingPricingForm.updateFailed" />
+          <FormattedMessage id="EditListingAboutForm.updateFailed" />
         </p>
       ) : null}
       {showListingsError ? (
         <p className={css.error}>
-          <FormattedMessage id="EditListingPricingForm.showListingFailed" />
+          <FormattedMessage id="EditListingAboutForm.showListingFailed" />
         </p>
       ) : null}
     </>
   );
 };
 
-/**
- * The EditListingPricingForm component.
- *
- * @component
- * @param {Object} props
- * @param {string} [props.formId] - The form id
- * @param {string} [props.className] - Custom class that extends the default class for the root element
- * @param {string} [props.rootClassName] - Custom class that overrides the default class for the root element
- * @param {string} props.unitType - The unitType from listing.attributes.publicData
- * @param {Object} [props.listingTypeConfig] - The listing type config that matches with listingType on publicData.
- * @param {Object} [props.listingTypeConfig.priceVariations] - The price variations config.
- * @param {boolean} props.listingTypeConfig.priceVariations.enabled - Whether the price variations are enabled.
- * @param {Object} [props.listingTypeConfig.transactionType] - The transaction type config.
- * @param {string} props.listingTypeConfig.transactionType.process - The transaction process config.
- * @param {string} props.marketplaceCurrency - The marketplace currency
- * @param {number} [props.listingMinimumPriceSubUnits] - The listing minimum price sub units
- * @param {boolean} [props.autoFocus] - Whether the input should be focused
- * @param {boolean} [props.disabled] - Whether the form is disabled
- * @param {boolean} [props.ready] - Whether the form is ready
- * @param {Function} props.onSubmit - The submit function
- * @param {boolean} [props.invalid] - Whether the form is invalid
- * @param {boolean} [props.pristine] - Whether the form is pristine
- * @param {string} props.saveActionMsg - The save action message
- * @param {boolean} [props.updated] - Whether the form is updated
- * @param {boolean} [props.updateInProgress] - Whether the form is updating
- * @param {Object} [props.fetchErrors] - The fetch errors
- * @returns {JSX.Element}
- */
 export const EditListingAboutForm = props => (
   <FinalForm
     mutators={{ ...arrayMutators }}
     {...props}
     render={formRenderProps => {
       const {
-        formId = 'EditListingPricingForm',
-        form: formApi,
-        autoFocus,
+        formId = 'EditListingAboutForm',
         className,
         rootClassName,
         disabled,
         ready,
         handleSubmit,
-        marketplaceCurrency,
-        unitType,
-        listingTypeConfig,
-        isPriceVariationsInUse,
-        listingMinimumPriceSubUnits = 0,
         invalid,
         pristine,
         saveActionMsg,
         updated,
         updateInProgress = false,
         fetchErrors,
-        initialValues: formInitialValues,
         values: formValues,
       } = formRenderProps;
 
       const intl = useIntl();
-      const priceValidators = getPriceValidators(
-        listingMinimumPriceSubUnits,
-        marketplaceCurrency,
-        intl
-      );
-
       const classes = classNames(rootClassName || css.root, className);
       const submitReady = (updated && pristine) || ready;
       const submitInProgress = updateInProgress;
       const submitDisabled = invalid || disabled || submitInProgress;
-      const { transactionType } = listingTypeConfig || {};
-      const { process } = transactionType || {};
-      const isBooking = isBookingProcess(process);
-
-      const isFixedLengthBooking = isBooking && unitType === FIXED;
-      const isBookingPriceVariationsInUse = isBooking && isPriceVariationsInUse;
-      const isUsingPriceVariants = isFixedLengthBooking || isBookingPriceVariationsInUse;
 
       return (
         <Form onSubmit={handleSubmit} className={classes}>
           <ErrorMessages fetchErrors={fetchErrors} />
 
-          <FieldTextInput
-            type="textarea"
-            id={formId ? `${formId}.about` : 'about'}
-            name="about"
-            className={css.inputBox}
-            autoComplete="pricingPolicy"
-            label={intl.formatMessage({
-              id: 'EditListingForm.aboutLabel',
-            })}
-            placeholder={intl.formatMessage({
-              id: 'EditListingForm.aboutPlaceholder',
-            })}
-            />
+          <h3 className={css.sectionTitle}>
+            <FormattedMessage id="EditListingAboutForm.aboutSectionTitle" defaultMessage="Add About Sections" />
+          </h3>
 
-          {isFixedLengthBooking ? (
-            <StartTimeInterval
-              name="startTimeInterval"
-              idPrefix={`${formId}_startTimeInterval`}
-              formValues={formValues}
-              pristine={pristine}
-            />
-          ) : null}
+          <FieldArray name="aboutSections">
+            {({ fields }) => (
+              <div className={css.fieldArrayWrapper}>
+                {fields.map((name, index) => (
+                  <div key={name} className={css.aboutSectionBox}>
+                    <h4 className={css.subTitle}>Section {index + 1}</h4>
+
+                    <div className={css.fieldRow}>
+                      <label className={css.fieldLabel}>Title</label>
+                      <Field
+                        name={`${name}.title`}
+                        component="input"
+                        type="text"
+                        placeholder="Enter section title"
+                        className={css.input}
+                        validate={validators.required('Title is required')}
+                      />
+                    </div>
+
+                    <div className={css.fieldRow}>
+                      <label className={css.fieldLabel}>Description</label>
+                      <FieldTextInput
+                        type="textarea"
+                        id={`${formId}.description_${index}`}
+                        name={`${name}.description`}
+                        placeholder="Enter details about this section"
+                        className={css.textarea}
+                        validate={validators.required('Description is required')}
+                      />
+                    </div>
+
+                    <button
+                      type="button"
+                      className={css.removeButton}
+                      onClick={() => fields.remove(index)}
+                    >
+                      Remove Section
+                    </button>
+                  </div>
+                ))}
+
+                <button
+                  type="button"
+                  className={css.addButton}
+                  onClick={() => fields.push({ title: '', description: '' })}
+                >
+                  + Add Another Section
+                </button>
+              </div>
+            )}
+          </FieldArray>
 
           <Button
             className={css.submitButton}
